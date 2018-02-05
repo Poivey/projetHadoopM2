883aa4438a6174daffcc865d9f3faf768f1a6fdc
Antoine Poivey <antoine.poivey@gmail.com>
2018-02-05 10:52:46 +0100

    refactor mapper2 with thomas's magic

--- src/main/java/fr/poivey/tache2/CrimeMapper.java
+++ src/main/java/fr/poivey/tache2/CrimeMapper.java
@@ -13,40 +13,24 @@ public class CrimeMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
   @Override
   public void setup(Context context) {
-    hourFilter = new Text[]{new Text("0-4"), new Text("4-8"), new Text("8-12"), new Text("12-16"), new Text("16-20"), new Text("20-24")};
+    hourFilter = new Text[]{new Text("0-4"), new Text("4-8"), new Text("8-12"), new Text("12-16"),
+        new Text("16-20"), new Text("20-24")};
   }
 
   @Override
-  public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
 
     if (key.get() != 0L) {
+
       String[] ligne = value.toString().split(",");
-
-      String fullHour = ligne[2].split(" ")[1].toLowerCase();
-
-      /**
-       * Converting am/pm to 24h system
-       */
-      int hour = Integer.valueOf(fullHour.split(":")[0])%12; //Midnight and noon become 0
-      if (ligne[2].split(" ")[2].toLowerCase().equals("pm")){
+      int hour =
+          Integer.valueOf(ligne[2].split(" ")[1].split(":")[0]) % 12; //Midnight and noon become 0
+      if (ligne[2].split(" ")[2].toLowerCase().equals("pm")) {
         hour += 12;
       }
-      String optionalZero = "";
-      if (hour < 10){optionalZero = "0";}
-      fullHour = optionalZero + String.valueOf(hour) + fullHour.substring(2);
 
-      /**
-       * Finding the time frame of the crime
-       */
-      Double hourDoubleValue = Double.valueOf(fullHour.toString().substring(0, 5).replace(":", "."));
-      Text category = new Text();
-      for (Text filter : hourFilter) {
-        if (category.toString().equals("") && hourDoubleValue <= Double.valueOf(filter.toString().split("-")[1])){
-          category = filter;
-        }
-      }
-
-      context.write(category, new IntWritable(1));
+      context.write(hourFilter[hour / 4], new IntWritable(1));
     }
 
   }
